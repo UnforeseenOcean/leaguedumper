3433e143d9b85503749734a4d10609cdf6e7fdc4
spudgy <antoniohermano@gmail.com>
2016-09-20 19:45:59 -0300

    added replay loading support

--- index.js
+++ index.js
@@ -11,10 +11,247 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 var bio = socketIo.listen(3001); //browser io
 var dio = socketIo.listen(3002); //daemon io
 
+
+const BinaryFile = require('binary-file');
+var ffi = require('ffi');
+var ref = require('ref');
+var smartBuffer = require('smart-buffer');
+var mcrypt = require('mcrypt');
+
+var intPtr = ref.refType('int');
+var Compression = ffi.Library('Compression.dll', {
+  'decompress': [ 'uint8*', [ 'string', 'int', intPtr ] ],
+  'free_mem': [ 'void', [] ]
+});
+var BlowfishLib = ffi.Library('BlowfishLib.dll', {
+	'BlowFishCreate': [ 'int', ['uint8*'] ],
+	'Decrypt': [ 'void', ['int','uint8*','int'] ]
+});
+
+function ParseReplay(file,addCallback,callback) {
+	var bf;
+	var html = '';
+	var json;
+	var buffer;
+	const myBinaryFile = new BinaryFile('./replay.lrf', 'r',true);
+	myBinaryFile.open().then(function () {
+	  console.log('File opened');
+	  return myBinaryFile.readUInt32();
+	}).then(function (version) {
+		html += 'Version: '+version+'<br>';
+		return myBinaryFile.readInt32();
+	}).then(function (jsonLen) {
+		return myBinaryFile.readString(jsonLen);
+	}).then(function (_json) {
+		json = JSON.parse(_json);
+		//bf = new Blowfish(json.encryptionKey);
+		bf = BlowfishLib.BlowFishCreate(Buffer.from(json.encryptionKey, 'base64'));
+		return myBinaryFile.size();
+	}).then(function (_size) {
+		var size = _size-myBinaryFile.tell();
+		return myBinaryFile.read(size);
+	}).then(function (_buffer) {
+		buffer = _buffer;
+	}).then(function () {
+		var vFrags = {};
+		var outNumber = ref.alloc('int'); // allocate a 4-byte (32-bit) chunk for the output datae
+		var fBuffer = Compression.decompress(buffer,buffer.length,outNumber);
+		var actualNumber = outNumber.deref();
+		var buf = new smartBuffer(fBuffer.reinterpret(actualNumber));
+		//do stuff?
+		var bDone = false;
+			  var packets = [];
+			  while(buf.remaining()) {
+				  var p = {};
+				  var packetTime = buf.readFloatLE();
+				  p.t = packetTime;
+				  p.l = buf.readUInt32LE();
+				  if(buf.remaining() < p.l) {
+					  console.log('zomg');
+					buf.readInt8();
+					p.t = buf.readFloatLE();
+				  	p.l = buf.readUInt32LE();
+				  }
+				  //skip enet?
+				  p.data = buf.readBuffer(p.l);
+				  p.GetHeader = function() { return this.data.readUInt8(0); };
+				  p.Parse = function() { 
+					  var buf = new smartBuffer(this.data);
+					  buf.readUInt32LE();
+					  buf.readUInt32LE();
+					  var nOpCode = buf.readInt8() & 0x0F;
+					  buf.readInt8();
+					  buf.readUInt16LE();
+					  if(nOpCode == 6) { //Reliable
+						  var nLen = buf.readInt16BE();
+						  if(nLen >= 8) {
+							  if(buf.remaining() < nLen || nLen == 8) {
+								return;
+							  }
+							  var _b = buf.readBuffer(nLen);
+							  BlowfishLib.Decrypt(bf,_b,nLen);
+							  var pBuf = new smartBuffer(_b);
+							  this.buffer = pBuf;
+							  var pHeader = pBuf.readUInt8();
+							  this.header = pHeader;
+							  if(pHeader == 0xFF) { //batched
+								  var nCount = pBuf.readUInt8();
+								  var nSize = pBuf.readUInt8();
+								  var nOpcode = pBuf.readUInt8();
+					  			  var netId = pBuf.readUInt32LE();
+								  
+								  var firstPacket = new Buffer(5);
+								  firstPacket.writeUInt8(nOpcode,0);
+								  firstPacket.writeUInt32LE(netId,1);
+								  if(nSize > 5) {
+									  if(pBuf.remaining() < nSize-5) {
+										 console.log('too big');
+										 }
+									  var payload = pBuf.readBuffer(nSize-5);
+									  firstPacket = Buffer.concat([firstPacket,payload],firstPacket.length+payload.length);
+								  }
+								  var obj = {header:nOpcode,data:firstPacket.toString('hex'),time:packetTime};
+								  obj.Debug = function() {
+									return 'B '+this.header.toString(16);
+								  }
+								  packets.push(obj);
+								  addCallback(obj);
+								  //iter
+								  for(var i = 1; i < nCount;i++) {
+									  var newId = 0;
+									  var netIdChanged = false;
+									  var command;
+									  var flagsAndLength = pBuf.readUInt8();
+									  var size = flagsAndLength >> 2;
+									  if ((flagsAndLength & 0x01) > 0) { // additionnal byte, skip command
+											command = nOpcode;
+											if ((flagsAndLength & 0x02) > 0) {
+												pBuf.readUInt8();
+											} else {
+												newId = pBuf.readUInt32LE();
+												netIdChanged = true;
+											}
+										}
+										else {
+											command = pBuf.readUInt8();
+											if ((flagsAndLength & 0x02) > 0) {
+												pBuf.readUInt8();
+											} else {
+												newId = pBuf.readUInt32LE();
+												netIdChanged = true;
+											}
+										}
+
+										if (size == 0x3F) {
+											size = pBuf.readUInt8(); // size is too big to be on 6 bits, so instead it's stored later
+										}
+									  var pBuffer = new Buffer(5);
+									  pBuffer.writeUInt8(command,0);
+									  if (netIdChanged) {
+											netId = newId;
+										}
+									  pBuffer.writeUInt32LE(netId,1);
+									  var payload = pBuf.readBuffer(size);
+									  pBuffer = Buffer.concat([pBuffer,payload],pBuffer.length+payload.length);
+									  //console.log(pBuffer);
+									  var obj = {header:command,data:pBuffer.toString('hex'),time:packetTime};
+									  obj.Debug = function() {
+										return 'B1 '+this.header.toString(16);
+									  }
+									  packets.push(obj);
+									  addCallback(obj);
+								  }
+							  } else if(pHeader == 0xFE) { //extended
+								  var obj = {header:pHeader,data:_b.toString('hex'),time:packetTime};
+								  obj.Debug = function() {
+									return 'Ext '+this.header.toString(16);
+								  }
+								  packets.push(obj);
+								  addCallback(obj);
+							  } else {
+								  var obj = {header:pHeader,data:_b.toString('hex'),time:packetTime};
+								  obj.Debug = function() {
+									return this.header.toString(16);
+								  }
+								  packets.push(obj);
+								  addCallback(obj);
+							  }
+						  }
+					  } else if(nOpCode == 8) { //Fragment
+						  var fragmentGroup = buf.readUInt16LE();
+						  var nLen = buf.readInt16BE();
+						  //check len?
+						  if(buf.remaining() < nLen+16) {
+							  console.log(buf.remaining()+' - fragment too big - '+nLen);
+							return;
+						  }
+						  //console.log('frag ok');
+						  var totalFragments  = buf.readUInt32BE();
+						  var currentFragment   = buf.readUInt32BE();
+						  var totalLen   = buf.readUInt32BE();
+						  buf.readUInt32BE(); //offset
+						  var payload = buf.readBuffer(nLen);
+						  //process payload
+						  var vFrag = vFrags[fragmentGroup] || {};
+						  vFrag[currentFragment] = payload;
+						  vFrags[fragmentGroup] = vFrag;
+						  if(Object.keys(vFrag).length == totalFragments) {
+							  var _b = new Buffer(vFrag[0]);
+							  for(var i = 1; i < vFrag.length;i++) {
+								  var payload = vFrag[i];
+									_b = Buffer.concat([_b,payload],_b.length+payload.length);
+							  }
+							  BlowfishLib.Decrypt(bf,_b,_b.length);
+							  var pBuf = new smartBuffer(_b);
+							  var obj = {header:pBuf.readUInt8(),data:_b.toString('hex'),time:packetTime};
+								  obj.Debug = function() {
+									return 'F '+this.header.toString(16);
+								  }
+								  packets.push(obj);
+							  addCallback(obj);
+							  //add packet
+							delete vFrags[fragmentGroup];
+						  }
+					  } else if(nOpCode == 0x0C) { //Count - unknown command...
+					  }
+				  };
+				  
+				  buf.readInt8();
+				  //check l?
+				  if(packets.length < 1000) {
+				  	p.Parse();
+				  }
+				  //packets.push(p);
+			  }
+			/*html += 'JSon: '+ json+'<br>';
+			html += 'Packets: '+ packets.length +'<br>';
+			for(var i = 0; i < packets.length;i++) {
+				html += packets[i].Debug()+'<br>';
+			}*/
+		
+		
+		Compression.free_mem();
+		callback(null,packets,json);
+		
+		return myBinaryFile.close();
+	}).catch(function (err) {
+	  console.log(`There was an error: ${err}`);
+		callback(err,null,null);
+	});
+}
+
 bio.on("connection", function(socket) { //BROWSER IO
     //console.log("new browser connection");
     socket.on('disconnect', function() {
     	//console.log("browser disconnect");
+    });
+    socket.on('parseReplay', function() {
+		ParseReplay('replay.lrf',function(obj) {
+			bio.emit('replayPacket',obj);
+		},function(err,packets,json) {
+			bio.emit('replayParsed');
+		});
+    	console.log('parsing replay..');
     });
 });
 dio.on("connection", function(socket) { //DAEMON IO
@@ -39,10 +276,10 @@ dio.on("connection", function(socket) { //DAEMON IO
 });
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname+'/home.html');
-});
-app.get('/hexer', function (req, res) {
 	res.sendFile(__dirname+'/hexedit.html');
+});
+app.get('/style.css', function (req, res) {
+	res.sendFile(__dirname+'/style.css');
 });
 app.get('/cfg', function (req, res) {
 	res.sendFile(__dirname+'/packetEnum.js');
--- packetParser/parser.js
+++ packetParser/parser.js
@@ -1,50 +1,146 @@
 var nIndex = 0;
-function AddByte(aData,name) {
+function AddByte(dv,aData,name) {
 	aData.push({ 'text': name,
 						   'a_attr': {'dataOffset':nIndex,'size':1},
 						   type: "root"
 						  });
+	console.log(dv);
+	var val = dv.getUint8(nIndex);
 	nIndex += 1;
+	return val;
 }
-function AddWord(aData,name) {
+function AddWord(dv,aData,name) {
 	aData.push({ 'text': name,
 						   'a_attr': {'dataOffset':nIndex,'size':2},
 						   type: "root"
 						  });
+	var val = dv.getUint16(nIndex,true);
 	nIndex += 2;
+	return val;
 }
-function AddDword(aData,name) {
+function AddDword(dv,aData,name) {
 	aData.push({ 'text': name,
 						   'a_attr': {'dataOffset':nIndex,'size':4},
 						   type: "root"
 						  });
+	var val = dv.getUint32(nIndex,true);
 	nIndex += 4;
+	return val;
 }
-function AddFloat(aData,name) {
+function AddFloat(dv,aData,name) {
 	aData.push({ 'text': name,
 						   'a_attr': {'dataOffset':nIndex,'size':4},
 						   type: "root"
 						  });
+	var val = dv.getFloat32(nIndex);
 	nIndex += 4;
+	return val;
+}
+function AddString(dv,aData,name) {
+	var nLen = 0;
+	while(dv.getUint8(nIndex+nLen) > 0) {
+		nLen++;	
+	}
+	nLen++;
+	aData.push({ 'text': name,
+						   'a_attr': {'dataOffset':nIndex,'size':nLen},
+						   type: "root"
+						  });
+	nIndex += nLen;
+	return 'string';
+}
+function AddField(dv,aData,name,len) {
+	aData.push({ 'text': name,
+						   'a_attr': {'dataOffset':nIndex,'size':len},
+						   type: "root"
+						  });
+	nIndex += len;
 }
 
 function ParsePacket(header,buffer,aData) {
 	var dataView = new DataView(buffer);
 	nIndex = 0;
+		AddByte(dataView,aData,'header');
+		AddDword(dataView,aData,'netId');
 	if(header == 7) {
-		AddByte(aData,'b1');
-		AddByte(aData,'b2');
-		//..
+	} else if(header == 0xB5) { //PKT_S2C_CastSpellAns
+		AddDword(dataView,aData,'clock');
+		AddByte(dataView,aData,'unk');
+		AddWord(dataView,aData,'wFlag');
+		AddDword(dataView,aData,'spellHash');
+		AddDword(dataView,aData,'spellNetId');
+		AddByte(dataView,aData,'unk');
+		AddFloat(dataView,aData,'unk');
+		AddDword(dataView,aData,'ownerNetId');
+		AddDword(dataView,aData,'ownerNetId');
+		AddDword(dataView,aData,'championHash');
+		AddDword(dataView,aData,'projectileNetId');
+		AddFloat(dataView,aData,'x');
+		AddFloat(dataView,aData,'y');
+		AddFloat(dataView,aData,'z');
+		AddFloat(dataView,aData,'x');
+		AddFloat(dataView,aData,'y');
+		AddFloat(dataView,aData,'z');
+		var vLen = AddByte(dataView,aData,'unk'); //len?
+		for(var i = 0; i < vLen;i++) {
+			AddDword(dataView,aData,'unkNedId');
+			AddByte(dataView,aData,'unkByte');
+		}
+		AddFloat(dataView,aData,'fChannelTime');
+		AddFloat(dataView,aData,'fDelay');
+		AddFloat(dataView,aData,'fVisible');
+		AddFloat(dataView,aData,'fCooldown');
+		AddFloat(dataView,aData,'unk');
+		AddByte(dataView,aData,'unk');
+		AddByte(dataView,aData,'slot');
+		AddFloat(dataView,aData,'cost');
+		AddFloat(dataView,aData,'ownerX');
+		AddFloat(dataView,aData,'ownerY');
+		AddFloat(dataView,aData,'ownerZ');
+		AddDword(dataView,aData,'1');
+		AddDword(dataView,aData,'0');
+	} else if(header == 0xB0) { //PKT_S2C_SpellAnimation
+		AddByte(dataView,aData,'unk');
+		AddDword(dataView,aData,'unk');
+		AddDword(dataView,aData,'unk');
+		AddFloat(dataView,aData,'animSpeed');
+		AddString(dataView,aData,'animName');
+	} else if(header == 0x6B) { //PKT_S2C_SetAnimation
+		var nCount = AddByte(dataView,aData,'count')*2;
+		for(var i = 0; i < nCount;i++) {
+			var nLen = AddDword(dataView,aData,'len');
+			AddField(dataView,aData,'anim',nLen);
+		}
+	} else if(header == 0x55) { //PKT_S2C_BlueTip
+		AddField(dataView,aData,'text',128);
+		AddField(dataView,aData,'title',256);
+		AddByte(dataView,aData,'bDelete');
+		AddDword(dataView,aData,'netId');
+	} else if(header == 0x6E) { //PKT_S2C_ShowProjectile
+		AddDword(dataView,aData,'netId');
+	} else if(header == 0x6F) { //PKT_S2C_BuyItemAns
+		AddDword(dataView,aData,'itemId');
+		AddByte(dataView,aData,'slot');
+		AddByte(dataView,aData,'stack');
+		AddByte(dataView,aData,'unk');
+		AddByte(dataView,aData,'unk');
+	} else if(header == 0x34) { //PKT_S2C_StopAutoAttack
+		AddDword(dataView,aData,'unk');
+		AddByte(dataView,aData,'unk');
+	} else if(header == 0x50) { //PKT_S2C_FaceDirection
+		AddByte(dataView,aData,'unk');
+		AddFloat(dataView,aData,'x');
+		AddFloat(dataView,aData,'y');
+		AddFloat(dataView,aData,'z');
+		AddFloat(dataView,aData,'timeToTurn');
 	} else if(header == 0x9A) {
-		AddByte(aData,'header');
-		AddDword(aData,'netId');
-		AddByte(aData,'spellSlotType');
-		AddByte(aData,'spellSlot');
-		AddFloat(aData,'x');
-		AddFloat(aData,'y');
-		AddFloat(aData,'x2');
-		AddFloat(aData,'y2');
-		AddDword(aData,'targetNetId');
+		AddByte(dataView,aData,'spellSlotType');
+		AddByte(dataView,aData,'spellSlot');
+		AddFloat(dataView,aData,'x');
+		AddFloat(dataView,aData,'y');
+		AddFloat(dataView,aData,'x2');
+		AddFloat(dataView,aData,'y2');
+		AddDword(dataView,aData,'targetNetId');
 			
 	}
 	if(nIndex < buffer.byteLength) {
